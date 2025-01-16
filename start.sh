a98394f75ebeea6eb41e9bb73656a5a8f440488d
sampbr <adrian.nx@hotmail.com>
2025-01-16 08:22:07 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,19 +9,29 @@ mostrar_ultimas_linhas_log() {
     if [ -f "$LOG_FILE" ]; then
         # Exibir as últimas 35 linhas do arquivo de log
         echo "Últimas 35 linhas do arquivo de log ($LOG_FILE):"
-        tail -n 35 "$LOG_FILE"
+        tail -n 50 "$LOG_FILE"
     else
         echo "Arquivo de log $LOG_FILE não encontrado."
     fi
 }
 
-# Função para verificar se o servidor está rodando e exibir logs
+# Função para verificar se o servidor está rodando
+verificar_servidor() {
+    # Verificar se o processo do servidor está em execução
+    if ! pgrep -x "samp03svr" > /dev/null; then
+        echo "O processo do servidor samp03svr não está mais rodando."
+        return 1  # Retorna 1 se o processo não estiver rodando
+    else
+        return 0  # Retorna 0 se o processo estiver rodando
+    fi
+}
+
+# Função para monitorar o servidor e mostrar os logs
 monitorar_servidor() {
     while true; do
-        # Verificar se o processo do servidor ainda está rodando
-        if ! pgrep -x "samp03svr" > /dev/null; then
-            echo "O servidor fechou automaticamente. Encerrando o script."
-            # Exibir as últimas 35 linhas de log quando o servidor parar
+        # Verificar se o servidor ainda está rodando
+        if ! verificar_servidor; then
+            echo "O servidor fechou automaticamente. Exibindo últimas 35 linhas do log."
             mostrar_ultimas_linhas_log
             exit 0
         fi
