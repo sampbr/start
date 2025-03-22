b0155483a19810b913fc8ebc8c6ec2bc82e492b7
sampbr <adrian.nx@hotmail.com>
2025-03-22 17:00:01 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,48 +1,37 @@
-#!/bin/bash
+#!/bin/sh
 
-# Caminho para o executável do servidor
+# Caminhos
 SERVER_PATH="./samp03svr"
 LOG_DIR="./logs"
-CFG_FILE="./server.cfg"
+LOG_FILE="server.log"
 
-# Verificar se o servidor já está em execução
-if pgrep -x "samp03svr" > /dev/null; then
-    echo "O servidor já está em execução. Não será iniciado novamente."
-    exit 1
-fi
-
-# Verificar se a pasta logs existe e removê-la
+# Remover pasta de logs se existir
 if [ -d "$LOG_DIR" ]; then
-    echo "Pasta logs identificada. Tentando remover..."
+    echo "Pasta logs identificada. Removendo..."
     rm -rf "$LOG_DIR"
-    
-    if [ ! -d "$LOG_DIR" ]; then
-        echo "Pasta logs removida com sucesso!"
-    else
-        echo "Falha ao remover a pasta logs. Verifique as permissões ou se a pasta está em uso."
-    fi
-else
-    echo "Pasta logs não encontrada. Nenhuma ação necessária."
 fi
 
-# Verificar se o arquivo samp03svr existe
+# Verificar se o executável do SAMP existe
 if [ ! -f "$SERVER_PATH" ]; then
-    echo "Arquivo $SERVER_PATH não encontrado. Baixando..."
-    
+    echo "Servidor não encontrado. Baixando..."
     curl -L https://github.com/sampbr/start/raw/main/samp03svr -o $SERVER_PATH
-    
-    chmod 755 $SERVER_PATH
-    
-    echo "Arquivo samp03svr baixado e permissões definidas!"
-else
-    current_permissions=$(stat -c "%a" "$SERVER_PATH")
-    if [ "$current_permissions" != "755" ]; then
-        echo "Permissões do arquivo $SERVER_PATH não estão como 755. Corrigindo..."
-        chmod 755 $SERVER_PATH
-        clear
-    fi
+    chmod +x $SERVER_PATH
 fi
 
-# Iniciar o servidor
+# Remover log anterior
+rm -f $LOG_FILE
+
+# Iniciar o servidor e capturar saída no log
 echo "Iniciando o servidor..."
-exec ./$SERVER_PATH
+./$SERVER_PATH > $LOG_FILE 2>&1 &
+
+# Aguardar indefinidamente até detectar "Started" no log
+echo "Aguardando o servidor iniciar..."
+
+while true; do
+    if grep -q "Started" $LOG_FILE; then
+        echo "Started"
+        break
+    fi
+    sleep 10
+done
